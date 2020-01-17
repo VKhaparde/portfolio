aa9a95ef1d28cac44e8421270939db5fa03aeb79
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-01-17 22:19:30 +0000

    Minor changes

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
       <div>
         <nav className="navbar navbar-expand-md fixed-top bg-dark navbar-dark">
           <a className="navbar-brand" href="#">Vandana Khaparde</a>
-          <button className="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
