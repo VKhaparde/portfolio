21c054300e3d7abcf66706664ff53908d9108ce4
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-01-21 16:25:25 +0000

    Made mobile responsive

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -5,29 +5,31 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
-          <a className="navbar-brand" href="#">Vandana Khaparde</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        <nav className="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Vandana Khaparde</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
           <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <li className="nav-item active" data-toggle="collapse" data-target=".navbar-collapse.show">
+                <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                 <a className="nav-link" href="#about">About Me</a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                 <a className="nav-link" href="#applications">Applications</a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                 <a className="nav-link" href="#skills">Skills</a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                 <a className="nav-link" href="#tools">Tools</a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
                 <a className="nav-link" href="#contact">Contact</a>
               </li>
             </ul>
